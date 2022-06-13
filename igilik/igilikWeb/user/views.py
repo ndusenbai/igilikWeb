ac30a9f1f38f9629f96ec3e0ffd9b31875d1a204
nurdaulet <ndusenbai@gmail.com>
2022-06-13 09:53:24 +0600

    add profile page

--- igilik/igilikWeb/user/views.py
+++ igilik/igilikWeb/user/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.urls import reverse_lazy
 
 class UserRegisterView(generic.CreateView):
     form_class = UserCreationForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
+
+class UserEditView(generic.CreateView):
+    form_class = UserChangeForm
+    template_name = 'registration/edit_profile.html'
+    success_url = reverse_lazy('home')
+
+    def get_object(self):
+        return self.request.user
